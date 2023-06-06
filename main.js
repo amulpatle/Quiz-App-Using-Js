760c1bbc68e139f487c42f3a43d5abb662367d82
amul patle <amulpatle3@gmail.com>
2023-06-07 00:27:12 +0530

    add basic style

---
+++ main.js
@@ -0,0 +1,28 @@
+const container = document.querySelector(".container");
+const question = document.querySelector(".question");
+const choices = document.querySelector(".choices");
+const nextBtn = document.querySelector(".nextBtn");
+
+const quiz = [
+  {
+    question: "Q. which of the following is not a CSS box model property?",
+    choices: ["margin", "padding", "border-radius", "border-collpase"],
+    answer: "border-collpase",
+  },
+  {
+    question:
+      "Q. which of the following is not a valid declare a function in javaScript?",
+    choices: ["margin", "padding", "border-radius", "border-collpase"],
+    answer: "border-collpase",
+  },
+  {
+    question: "Q. which of the following is not a CSS box model property?",
+    choices: ["margin", "padding", "border-radius", "border-collpase"],
+    answer: "border-collpase",
+  },
+  {
+    question: "Q. which of the following is not a CSS box model property?",
+    choices: ["margin", "padding", "border-radius", "border-collpase"],
+    answer: "border-collpase",
+  },
+];
